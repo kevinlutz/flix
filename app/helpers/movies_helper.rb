016e1f3cfbedcc889d412bcab660e3fee1c9b734
kevinlutz <kevinlutz@gmail.com>
2021-05-04 10:28:05 -0400

    Added helper methods

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,22 @@ module MoviesHelper
             number_to_currency(movie.price, precision: 0)
         end
     end
+
+    def total_gross(movie)
+        if movie.flop?
+          "Flop!"
+        else
+          number_to_currency(movie.total_gross, precision: 0)
+        end
+    end
+
+    def year_of(movie)
+        if movie.released_on.nil?
+            "No Date"
+        else
+            movie.released_on.year
+        end
+    end
+
+    
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,15 @@
 class Movie < ApplicationRecord
 
     def free?
-        price.zero? || price.empty?
+        price.zero? || price.blank?
+    end
+
+    def showtime
+        movie.show_at.strftime("%B %d at %I:%M %P")
+    end
+
+    def flop?
+        total_gross.blank? || total_gross < 225_000_000
     end
 
 end
