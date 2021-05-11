9a27e9e7194ec9dff4bceadee3bd8558cd5d3404
kevinlutz <kevinlutz@gmail.com>
2021-05-10 21:49:35 -0400

    Changed the truncate description length

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root "movies#index"
 
   get "movies" => "movies#index"
+  get "events/new" => "events#new"
   get "movies/:id" => "movies#show", as: "movie"
   get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   patch "movies/:id" => "movies#update"
+  
 
 end
